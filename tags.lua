6cef325c30e211f2903ef9640993b7c0978478fc
WhitelistBot1 <novolineconfigs@gmail.com>
2024-10-22 20:41:33 -0700

    Create tags.lua

---
+++ tags.lua
@@ -0,0 +1,6 @@
+return {
+    [1] = {name = "ABYSS OWNER 👑", color = Color3.fromRGB(131, 52, 235)},
+    [2] = {name = "DEVELOPER 🛠️", color = Color3.fromRGB(0, 140, 255)},
+    [3] = {name = "⭐", color = Color3.fromRGB(255, 179, 0)},
+    [4] = {name = "VAPE PRIVATE", color = Color3.fromRGB(255, 76.5, 76.5)},
+}
