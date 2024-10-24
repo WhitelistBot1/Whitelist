4dce78356ebdf3132ae295b4425b57d5548bd063
WhitelistBot1 <novolineconfigs@gmail.com>
2024-10-24 12:39:31 -0700

    fixed abyss private color

--- tags.lua
+++ tags.lua
@@ -2,5 +2,5 @@ return {
     [1] = {name = "ABYSS OWNER 👑", color = Color3.fromRGB(131, 52, 235)},
     [2] = {name = "DEVELOPER 🛠️", color = Color3.fromRGB(0, 140, 255)},
     [3] = {name = "⭐", color = Color3.fromRGB(255, 179, 0)},
-    [4] = {name = "ABYSS PRIVATE", color = Color3.fromRGB(255, 76.5, 76.5)},
+    [4] = {name = "ABYSS PRIVATE", color = Color3.fromRGB(255, 76, 76)},
 }
