e5947205a00471976e963805ebe2c583208a4503
AbyssForRoblox <9dv8jqda@mailmask.me>
2024-10-31 21:31:36 -0700

    Update tags.lua

--- tags.lua
+++ tags.lua
@@ -1,6 +1,7 @@
 return {
-    [1] = {name = "ABYSS OWNER 👑", color = Color3.fromRGB(131, 52, 235)},
+    [0] = {name = "ABYSS USER 👍", color = Color3.fromRGB(255,50,0)},
+    [1] = {name = "ABYSS OWNER 👑", color = Color3.fromRGB(255, 50, 0)},
     [2] = {name = "DEVELOPER 🛠️", color = Color3.fromRGB(0, 140, 255)},
-    [3] = {name = "⭐", color = Color3.fromRGB(255, 179, 0)},
-    [4] = {name = "ABYSS PRIVATE", color = Color3.fromRGB(255, 76, 76)},
+    [3] = {name = "⭐ STAR", color = Color3.fromRGB(255, 179, 0)},
+    [4] = {name = "ABYSS PRIVATE 🤑", color = Color3.fromRGB(255, 76, 76)},
 }
