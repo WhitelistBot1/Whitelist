2af180a4f1aa694ba95652da8b636fc847b31019
AbyssForRoblox <9dv8jqda@mailmask.me>
2024-10-31 21:39:29 -0700

    Update whitelisted.lua

--- whitelisted.lua
+++ whitelisted.lua
@@ -1,5 +1,5 @@
 return {
     {name = game.Players.LocalPlayer.Name, tags = {0}},
     {name = "TheFakestStarfr", tags = {2}},
-    {name = "IDESTROYASSKIDSONLY", tags = {1, 2}}, -- IGNORE IT I ABUSE ROBLOX MODERATION
+    {name = "IDESTROYASSKIDSONLY", tags = {1, 2}} -- IGNORE IT I ABUSE ROBLOX MODERATION
 }
