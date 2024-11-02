b82a75fc8fda87c31f35cba87ae4181d36f8c10d
AbyssForRoblox <9dv8jqda@mailmask.me>
2024-11-01 20:09:35 -0700

    Fixed tag

--- tags.lua
+++ tags.lua
@@ -1,5 +1,5 @@
 return {
-    [0] = {name = "ABYSS USER 👍", color = Color3.fromRGB(255,50,0)},
+    [0] = {name = "ABYSS USER 👍", color = Color3.fromRGB(255, 50, 0)},
     [1] = {name = "ABYSS OWNER 👑", color = Color3.fromRGB(255, 50, 0)},
     [2] = {name = "DEVELOPER 🛠️", color = Color3.fromRGB(0, 140, 255)},
     [3] = {name = "⭐ STAR", color = Color3.fromRGB(255, 179, 0)},
